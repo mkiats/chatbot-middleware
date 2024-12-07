b0cd1ec6ecc0e7053c554d9d1927132b8a1e2080
mkiats <mkiats>
2024-12-07 20:27:41 +0800

    save state

--- telegram-azure/entities.py
+++ telegram-azure/entities.py
@@ -1,12 +1,50 @@
-def callback_data_chatbot(chatbot_uuid: str):
-    return {
-        'command': 'command_callback_selectChatbot',
-        'chatbot_uuid': chatbot_uuid
-    }
+from typing import Optional
+from uuid import UUID
+import re
 
-def inline_keyboard_button(text: str, callback_data: str = None) -> object:
-    json_obj = {
-        'text': text,
-        'callback_data': text
-    }
-    return json_obj
+# def callback_data_chatbot(chatbot_uuid: str):
+#     return {
+#         'command': 'command_callback_selectChatbot',
+#         'chatbot_uuid': chatbot_uuid
+#     }
+
+
+class ChatbotCallbackData:
+    def __init__(self, command: str, chatbot_uuid: str):
+        self.command = command
+        self.chatbot_uuid = chatbot_uuid
+        
+    def to_dict(self) -> dict:
+        return vars(self)
+
+
+class Chatbot:
+    def __init__(self, chatbot_uuid: str, chatbot_name: str, chatbot_endpoint: str, chatbot_status: str):
+        self.chatbot_uuid = chatbot_uuid
+        self.chatbot_name = chatbot_name
+        self.chatbot_endpoint = chatbot_endpoint
+        self.chatbot_status = chatbot_status
+
+    def __post_init__(self):
+        self.validate()
+    
+    def validate(self) -> bool:
+        # Populate with custom error exceptions, throw customException when necessary
+        # Need to handle chatbot_uuid should be UUID format, endpoint needs to be valid
+            
+        if self.status not in ['active', 'maintenance', 'deprecated']:
+            raise ValueError("status must be one of: active, maintenance, deprecated")
+            
+        return True
+    
+    def to_dict(self) -> dict:
+        return vars(self)
+    
+class inline_keyboard_button:
+    def __init__(self, text: str, callback_data: ChatbotCallbackData = None):
+        self.text = text
+        self.callback_data = ChatbotCallbackData
+
+    def to_dict(self) -> dict:
+        return vars(self)
+    
--- telegram-azure/function_app.py
+++ telegram-azure/function_app.py
@@ -1,17 +1,14 @@
-import azure.functions as func
 from azure.cosmos import CosmosClient, PartitionKey
-from azure.identity import DefaultAzureCredential
+from api_telegram import command_telegram_gateway
+from dotenv import load_dotenv
+import azure.functions as func
 import logging
 import os
-import httpx
-import json
-from dotenv import load_dotenv
-from entities import callback_data_chatbot, inline_keyboard_button
-# from api import start_command, list_command
 
+# Instantiate function app
 load_dotenv()
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-credentials = DefaultAzureCredential()
+# Instantiate cosmos db
 client = CosmosClient.from_connection_string(os.getenv("COSMOS_DB_CONNECTION_STRING"))
 user_database = client.create_database_if_not_exists("UserDB")
 user_container = user_database.create_container_if_not_exists(id="users", partition_key=PartitionKey(path="/user_uuid"))
@@ -19,38 +16,11 @@ chatbot_database = client.create_database_if_not_exists("ChatbotDB")
 chatbot_container = chatbot_database.create_container_if_not_exists(id="chatbots", partition_key=PartitionKey(path="/chatbot_uuid"))
 
 
-
+# Instantiate routes
 @app.route(route="processTelegramMessage")
 async def processTelegramMessage(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function "processTelegramMessage" processed a request.')
-
-    payload = req.get_json()
-    logging.info(f'\n Payload:\n{payload}\n')
-    [message, chat_id, text, callback_query, callback_data] = destructure_json_response(payload)
-
-
-    if payload:
-        try:
-            if text=="/start":
-                await start_command(chat_id)
-            elif text=="/list":
-                await list_command(chat_id)
-            else:
-                await echo_message(chat_id, callback_data)
-                logging.info(f'Unknown command detected...')
-            
-            return func.HttpResponse(
-            f"This HTTP triggered function executed successfully, text is {text}", 
-            status_code=200
-            )
-        except:
-            logging.info(f'Error in sending response...')
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-    
+    return await command_telegram_gateway(req=req, user_container=user_container, chatbot_container=chatbot_container)
 
 
 @app.route(route="processTempMessage", auth_level=func.AuthLevel.ANONYMOUS)
@@ -63,79 +33,21 @@ def processTempMessage(req: func.HttpRequest) -> func.HttpResponse:
     )
 
 
+@app.route(route="chatbotv1", auth_level=func.AuthLevel.ANONYMOUS)
+def chatbotv1(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    return func.HttpResponse(
+            "Chatbotv1 called...",
+            status_code=200
+    )
 
 
+@app.route(route="chatbotv2", auth_level=func.AuthLevel.ANONYMOUS)
+def chatbotv2(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
 
-
-
-
-async def execute_url(method, params="", json=""):
-    with httpx.Client() as client:
-        URL = os.getenv("TELEGRAM_API_URL")
-        TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-
-        try:
-            if params:
-                logging.info(f'\nSendMessage params payload:\n {params}\n')
-            if json:
-                logging.info(f'\nSendMessage json payload:\n {json}\n')
-            response = client.post(f'{URL}{TOKEN}/{method}', params=params, json=json)
-            # logging.info(f"executing {method}: {response}")
-            return response
-        except:
-            logging.error(f'Error in executing {method}')
-
-
-
-async def echo_message(chat_id: str, text: str) -> None:
-
-    response_payload = {
-        'chat_id': chat_id,
-        'text': text
-        }
-    response = await execute_url("sendMessage", json=response_payload)
-
-async def start_command(chat_id: str) -> None:
-    
-    start_msg = f"Welcome to Chatbot marketplace!\nType /list to see available chatbots"
-    await echo_message(chat_id, text=start_msg)
-    pass
-
-async def list_command(chat_id: str) -> None:
-    
-    text_msg = f'The following chatbots are available'
-    chatbot1 = inline_keyboard_button("CS chatbot")
-    chatbot2 = inline_keyboard_button("HR chatbot")
-    chatbot3 = inline_keyboard_button("Post-sales chatbot")
-    inline_keyboard = {
-        'inline_keyboard': 
-            [[chatbot1],
-            [chatbot2],
-            [chatbot3]]
-        }
-    response_payload = {
-        'chat_id': chat_id,
-        'text': text_msg,
-        'reply_markup': inline_keyboard
-        }
-    response = await execute_url("sendMessage", json=response_payload)
-
-    pass
-
-
-
-def destructure_json_response(json_request: str) -> [str]:
-    message = json_request.get('message', None)
-    callback_query = json_request.get('callback_query', None)
-    chat_id = None
-    text = None
-    callback_data = None
-
-    if message:
-        chat_id = message['chat']['id']
-        text = message['text']
-    if callback_query:
-        chat_id = callback_query['message']['chat']['id']
-        callback_data = callback_query['data']
-
-    return [message, chat_id, text, callback_query, callback_data]
+    return func.HttpResponse(
+            "Chatbotv2 called...",
+            status_code=200
+    )
