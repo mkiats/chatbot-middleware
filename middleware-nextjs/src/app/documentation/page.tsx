b8606d70c83b8423c43a3787045659662d3c218e
mkiats <mkiats>
2025-02-09 17:26:38 +0800

    rename parent folder

--- middleware-nextjs/src/app/documentation/page.tsx
+++
@@ -1,6 +0,0 @@
-const DocumentationPage = () => {
-     return <div> DocumentationPage
-     </div>
-};
-
-export default DocumentationPage
--- middleware-nextjs/src/components/deployment/chatbotDeployment.tsx
+++
@@ -1,6 +0,0 @@
-const ChatbotDeployment = () => {
-    
-     return <div> ChatbotDeployment </div>
-};
-
-export default ChatbotDeployment
